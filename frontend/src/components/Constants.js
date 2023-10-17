f6695f5ea6e5ca2214dc592228399ac69da01d76
unknown <deepakchacko2700@gmail.com>
2023-10-17 09:18:05 +0530

    run build

--- frontend/src/components/Constants.js
+++ frontend/src/components/Constants.js
@@ -1,2 +1,2 @@
-// export const API_URL =  'https://dclively.pythonanywhere.com' // Production
-export const API_URL= 'http://127.0.0.1:8000' // Development
+export const API_URL =  'https://fun-chat-2s6u.onrender.com' // Production
+// export const API_URL= 'http://127.0.0.1:8000' // Development
